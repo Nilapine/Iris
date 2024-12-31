b14548c303e94e173f8c94d92a321551e20f5daa
bos ketcielz <113289837+Nilapine@users.noreply.github.com>
2024-12-31 10:48:54 +0700

    Update home.py

--- Tabs/home.py
+++ Tabs/home.py
@@ -17,11 +17,11 @@ def app(df, x, y):
     df.drop('Id', axis=1, inplace=True)
     st.write(df)
 
-    # Menambahkan Gambar Iris
-    st.image([
-        "https://upload.wikimedia.org/wikipedia/commons/4/41/Iris_setosa2.jpg",
-        "https://upload.wikimedia.org/wikipedia/commons/a/a7/Iris_versicolor_3.jpg",
-        "https://upload.wikimedia.org/wikipedia/commons/9/9f/Iris_virginica.jpg"
-    ], caption=["Iris Setosa", "Iris Versicolor", "Iris Virginica"], width=300)
-
-    
+    # Menambahkan Gambar Iris Berjejeran
+    col1, col2, col3 = st.columns(3)
+    with col1:
+        st.image("https://upload.wikimedia.org/wikipedia/commons/4/41/Iris_setosa2.jpg", caption="Iris Setosa", width=150)
+    with col2:
+        st.image("https://upload.wikimedia.org/wikipedia/commons/a/a7/Iris_versicolor_3.jpg", caption="Iris Versicolor", width=150)
+    with col3:
+        st.image("https://upload.wikimedia.org/wikipedia/commons/9/9f/Iris_virginica.jpg", caption="Iris Virginica", width=150)
