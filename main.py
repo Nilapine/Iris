51450a7419830fb72d759dc02907fe0ec4b1ee47
bos ketcielz <113289837+Nilapine@users.noreply.github.com>
2024-12-31 11:07:32 +0700

    Update main.py
    update background

--- main.py
+++ main.py
@@ -1,26 +1,47 @@
 import streamlit as st
 from web_functions import load_data
-
-
 from Tabs import home, predict, visualise
 
+# Fungsi untuk menambahkan latar belakang gambar
+def set_background_image(image_url):
+    st.markdown(
+        f"""
+        <style>
+        .stApp {{
+            background-image: url("{image_url}");
+            background-size: cover;
+            background-repeat: no-repeat;
+            background-attachment: fixed;
+        }}
+        </style>
+        """,
+        unsafe_allow_html=True
+    )
+
+# URL gambar dari GitHub
+background_image_url = "https://github.com/Nilapine/Iris/blob/main/background.png?raw=true"
+
+# Set latar belakang
+set_background_image(background_image_url)
+
+# Membuat Tabs
 Tabs = {
     "Home": home,
-    "Prediction" : predict,
-    "Visualisation" : visualise,
+    "Prediction": predict,
+    "Visualisation": visualise,
 }
 
-#membuat sidebar
+# Membuat Sidebar
 st.sidebar.title("Navigasi")
 
-#membuat radio option
-page = st.sidebar.radio("Pages",list(Tabs.keys()))
+# Membuat Radio Option
+page = st.sidebar.radio("Pages", list(Tabs.keys()))
 
+# Load Dataset
+df, x, y = load_data()
 
-#load dataset
-df,x,y = load_data()
-#kondisi call app function
-if page in ["Prediction","Visualisation"]:
-    Tabs[page].app(df,x,y)
+# Kondisi Call App Function
+if page in ["Prediction", "Visualisation"]:
+    Tabs[page].app(df, x, y)
 else:
-    Tabs[page].app(df,x,y) # type: ignore
+    Tabs[page].app(df, x, y)  # type: ignore
